2f5c7f32959cf50a1b91d3b1c1172b14f6ceeb6e
Eden Li <eden.li@gmail.com>
2012-02-16 09:47:05 -0800

    protobuf-objc: point to latest stable commit

--- Library/Formula/protobuf-objc.rb
+++ Library/Formula/protobuf-objc.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class ProtobufObjc < Formula
   homepage 'https://github.com/booyah/protobuf-objc'
-  url 'https://github.com/booyah/protobuf-objc/tarball/master'
+  url 'https://github.com/booyah/protobuf-objc/tarball/9eb586b2178163c2fdbdfce772a48731e5884905'
   md5 '596876aebf1f441360f985a7b5451a83'
   version '0.1.0'
 
